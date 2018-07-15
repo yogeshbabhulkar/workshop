ca8cefd1f88c7e611033dc9259875e237c1001d7
yogeshbabhulkar <yogeshbabhulkar>
2018-07-15 16:02:14 +0530

    My second contribution
    Signed-off-by: yogeshbabhulkar <yogeshbabhulkar>

---
+++ mycalci.py
@@ -0,0 +1,13 @@
+class calc():
+        
+        def add(a,b):
+                print("addition:",a+b)
+                       
+        def sub(a,b):
+                print("substraction:",a-b)       
+                
+        def mul(a,b):
+                print("multiplication:",a*b)
+                
+        def div(a,b):
+                print("division:",a/b)                      
